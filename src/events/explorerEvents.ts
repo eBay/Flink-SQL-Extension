58fd53a46a785128ce5838b4402478c7f3f48a1c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-17 19:55:40 +0800

    Delete src directory

--- src/events/explorerEvents.ts
+++
@@ -1,5 +0,0 @@
-import * as vscode from 'vscode';
-
-// event to trigger Monitor Explorer refresh
-export const refreshTaskManagersEvent = new vscode.EventEmitter<void>();
-export const refreshJobsEvent = new vscode.EventEmitter<void>();
